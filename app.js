4a7f7afc3478ca5bfe4e78eb4c4ab162528b8793
arjuaman <aman.arju427742@gmail.com>
2023-09-22 16:25:32 +0530

    Methods

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
     el: "#root",
     data: {
-        name: "Arju"
+        name: "Arju",
+        age: 25,
+        web1: "https://www.google.com",
+        web2: '<a href="https://www.google.com"> Holy grail! </a>'
+    },
+    methods: {
+        greet: function(){
+            return "Morning!";
+        }
     }
 })
