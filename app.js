3f4c0d4e8caa5e2933dc5ef7287086adc4f71d41
arjuaman <aman.arju427742@gmail.com>
2023-09-25 11:07:11 +0530

    keyboard events and keyboard modifiers

--- app.js
+++ app.js
@@ -3,27 +3,17 @@ new Vue({
     data: {
         name: "Arju",
         age: 25,
-        web1: "https://www.google.com",
-        web2: '<a href="https://www.google.com"> Holy grail! </a>',
-        x: 0,
-        y: 0,
+        city: "Patna"
     },
     methods: {
-        greet: function(name){
-            return "Good Morning! to "+name+" whose age is: "+this.age;
+        logName: function(){
+            console.log("Name entered");
         },
-        add: function(val=1){
-            this.age += val;
+        logAge: function(){
+            console.log("Age entered");
         },
-        subtract: function(val=1){
-            this.age -= val;
-        },
-        updateXY: function(e){
-            this.x = e.offsetX;
-            this.y = e.offsetY;
-        },
-        click: function(){
-            alert("Sup");
+        logCity: function(){
+            console.log("City entered");
         }
     }
 })
