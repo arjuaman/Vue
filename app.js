3c5dc6261bc09b099620492a757fe39278853c22
arjuaman <aman.arju427742@gmail.com>
2023-09-25 10:42:18 +0530

    v-on and @

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ new Vue({
         name: "Arju",
         age: 25,
         web1: "https://www.google.com",
-        web2: '<a href="https://www.google.com"> Holy grail! </a>'
+        web2: '<a href="https://www.google.com"> Holy grail! </a>',
+        x: 0,
+        y: 0,
     },
     methods: {
         greet: function(name){
             return "Good Morning! to "+name+" whose age is: "+this.age;
+        },
+        add: function(val=1){
+            this.age += val;
+        },
+        subtract: function(val=1){
+            this.age -= val;
+        },
+        updateXY: function(e){
+            this.x = e.offsetX;
+            this.y = e.offsetY;
         }
     }
 })
