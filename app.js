e121d3fbc5cafc489372b6daf111b5499245116e
arjuaman <aman.arju427742@gmail.com>
2023-09-25 14:28:44 +0530

    v-for for array

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 new Vue({
-    el: "#root",
+    el: '#root',
     data: {
-        error: false,
-        success: false
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            { name: 'Ryu', age: 25 },
+            { name: 'Yoshi', age: 35 },
+            { name: 'Ken', age: 55 }
+        ]
     },
     methods: {
-        
+
     },
     computed: {
-           
+
     }
-})
+});
