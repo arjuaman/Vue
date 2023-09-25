659b7d3877e97258d089838473110d35ba047041
arjuaman <aman.arju427742@gmail.com>
2023-09-25 16:49:09 +0530

    Better code

--- app.js
+++ app.js
@@ -5,20 +5,18 @@ new Vue({
         ended: false
     },
     methods: {
-        decreaseHealth: function(val){
-            if(this.health > 0)
-                this.health -= val;
-            else{
-                this.health = 0;
+        punch: function(){
+            this.health -= 10;
+            if ( this.health <= 0 ){
                 this.ended = true;
             }
         },
-        restartGame: function(){
+        restart: function(){
             this.health = 100;
             this.ended = false;
         }
     },
     computed: {
-        
+
     }
 });
