e89395bc8e5243d23440bfb04de332df09884cbe
arjuaman <aman.arju427742@gmail.com>
2023-09-26 10:51:07 +0530

    Refs

--- app.js
+++ app.js
@@ -1,46 +1,52 @@
-var datum = {
-    boss: "Oogway",
-    disciple: "Shifu"
-}
+// // var datum = {
+// //     boss: "Oogway",
+// //     disciple: "Shifu"
+// // }
 
-Vue.component("greetcomponent",{
-    template: '<div>{{boss}} passes the baton to {{disciple}} <button v-on:click.once="changeName">Change Name</button></div>',
-    data: function(){
-        return datum;
-    },
-    methods: {
-        changeName: function(){
-            this.boss = "Mario",
-            this.disciple = "Luigi"
-        }
-    }
-});
+// Vue.component("greetcomponent",{
+//     template: '<div>{{boss}} passes the baton to {{disciple}} <button v-on:click.once="changeName">Change Name</button></div>',
+//     data: function(){
+//         return{
+//             boss: "Oogway",
+//             disciple: "Shifu"
+//         }
+//     },
+//     methods: {
+//         changeName: function(){
+//             this.boss = "Mario",
+//             this.disciple = "Luigi"
+//         }
+//     }
+// });
 
 var one = new Vue({
     el: '#root1',
     data: {
-        
+        anime: "Naruto"
     },
     methods: {
-        
+        readTheRefs: function(){
+            console.log(this.$refs);
+            this.anime = this.$refs.inputRef.value;
+        }
     },
     computed: {
-
+        
     }
 });
 
-var two = new Vue({
-    el: '#root2',
-    data: {
+// var two = new Vue({
+//     el: '#root2',
+//     data: {
         
-    },
-    methods: {
+//     },
+//     methods: {
         
-    },
-    computed: {
+//     },
+//     computed: {
 
-    }
-});
+//     }
+// });
 
 // Can be done from anywhere like a nomral JS script too, example:
 // one.title = "Childe"
