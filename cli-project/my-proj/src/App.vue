3dc5ba6b289495cf00f549a73063a9e40874f254
Arju Aman <33257286+arjuaman@users.noreply.github.com>
2023-09-28 01:34:46 +0530

    use in templating

--- cli-project/my-proj/src/App.vue
+++ cli-project/my-proj/src/App.vue
@@ -1,26 +1,45 @@
 <template>
   <div>
-    <form-helper>
-      <h2 slot="title">{{creator}} is the GOAT</h2>
-      <p slot="text">A para tag</p>
-    </form-helper>
+      <form-helper>
+          <div slot="form-header">
+              <h3>This is the title of a form</h3>
+              <p>This is some info about the form</p>
+          </div>
+          <div slot="form-fields">
+              <input type="text" placeholder="name" required />
+              <input type="password" placeholder="password" required />
+          </div>
+          <div slot="form-controls">
+              <button v-on:click="handleSubmit">Submit</button>
+          </div>
+      </form-helper>
   </div>
 </template>
 
 <script>
-  import FormHelper from './components/FormHelper.vue';
+// Imports
+import FormHelper from './components/FormHelper.vue'
 
-  export default {
-    components: {
-      'form-helper': FormHelper,
-    },
-    data() {
+export default {
+  components: {
+      'form-helper': FormHelper
+  },
+  data () {
       return {
-        creator: "Oda Sensei"
-      }
-    },
-    methods: {
 
-    }
+      }
+  },
+  methods: {
+      handleSubmit: function(){
+          alert('thanks for submitting');
+      }
   }
+}
 </script>
+
+<style>
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
