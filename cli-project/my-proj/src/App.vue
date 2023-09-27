d25669dc717591059f422b6cc673da80a7fd808b
arjuaman <aman.arju427742@gmail.com>
2023-09-27 11:16:55 +0530

    Sample project using nested components

--- cli-project/my-proj/src/App.vue
+++ cli-project/my-proj/src/App.vue
@@ -1,30 +1,34 @@
 <template>
   <div>
-    <h3>{{title}}</h3>
-    <mangas></mangas>
+      <app-header></app-header>
+      <app-ninjas></app-ninjas>
+      <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import Manga from './Manga.vue'
+// Imports
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
 
 export default {
   components: {
-    'mangas': Manga
+      'app-header': Header,
+      'app-footer': Footer,
+      'app-ninjas': Ninjas
   },
   data () {
-    return {
-      title: "Animeverse"
-    }
-  },
-  methods: {
-    
+      return {
+
+      }
   }
 }
 </script>
 
-<style scoped>
-  div {
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  }
+<style>
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
 </style>
---
+++ cli-project/my-proj/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+<footer>
+    <h1>{{copyright}}</h1>
+</footer>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            copyright: "MIT License"
+        }
+    }
+}
+</script>
+
+<style scoped>
+footer{
+    background: black;
+    padding: 10px;
+}
+h1{
+    color: white;
+    text-align: center;
+}
+</style>
