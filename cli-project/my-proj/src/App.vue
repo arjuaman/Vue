17d66a42d5442313b06cb358bc140603fa8b1385
arjuaman <aman.arju427742@gmail.com>
2023-09-28 10:49:19 +0530

    keep-alive tag

--- cli-project/my-proj/src/App.vue
+++ cli-project/my-proj/src/App.vue
@@ -3,7 +3,9 @@
       <!-- <form-one></form-one>
       <form-two></form-two> -->
       <!-- <component is="form-one"></component> -->
-      <component v-bind:is="showComp"></component>
+      <keep-alive>
+        <component v-bind:is="showComp"></component>
+      </keep-alive>
       <button v-on:click="showComp = 'form-one'">Show form 1</button>
       <button v-on:click="showComp = 'form-two'">Show form 2</button>
   </div>
