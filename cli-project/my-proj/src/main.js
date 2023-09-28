c35b9ff83d7c60adff3e25797fc8573c7234d845
arjuaman <aman.arju427742@gmail.com>
2023-09-28 17:03:34 +0530

    Custom directive

--- cli-project/my-proj/src/main.js
+++ cli-project/my-proj/src/main.js
@@ -4,6 +4,12 @@ import VueResource from 'vue-resource';
 
 Vue.use(VueResource);
 
+Vue.directive('greenHeading',{
+  bind(el, binding, vnode){
+    el.style.color = "#427462";
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
