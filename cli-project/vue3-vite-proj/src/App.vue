12eac6334831dfc5b30caf761f422682788ed1c4
arjuaman <aman.arju427742@gmail.com>
2023-10-03 13:04:38 +0530

    Binding on key attribute when iterating over different sorts of arrays

--- cli-project/vue3-vite-proj/src/App.vue
+++ cli-project/vue3-vite-proj/src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <p v-if="num === 0">This is zero</p>
-  <p v-else-if="num < 0">Negative number</p>
-  <p v-else-if="num > 0">Positive number</p>
-  <p v-else>Not a number</p>
-
-  <p v-show="num === 0">This is zero</p>
-  <p v-show="num !== 0">This is non-zero</p>
+  <ul>
+    <li v-for="i in heroes" v-bind:key="i">{{i}}</li>
+  </ul>
+  <!-- <div> -->
+    <div v-for="i in ninjas" v-bind:key="i.name">{{i.name}} {{i.age}}</div>
+  <!-- </div> -->
+  <div>
+    <div v-for="person in actors" v-bind:key="person.name">
+      {{person.name}}
+      <p v-for="j in person.movies" v-bind:key="j">{{j}}</p>
+    </div>
+  </div>
+  <h3 v-for="(val, key, ind) in myInfo" :key="val">{{val}} {{key}} {{ind}}</h3>
 </template>
 
 <script>
@@ -13,7 +19,31 @@ export default {
   name: 'App',
   data() {
     return{
-      num: "p"
+      heroes: ["Clark","Dent","Logan"],
+      ninjas: [
+            { name: 'Ryu', age: 25 },
+            { name: 'Yoshi', age: 35 },
+            { name: 'Ken', age: 55 }
+        ],
+      actors: [
+        {
+          name: 'Bale',
+          movies: ['Batman', 'Prestige']
+        },
+        {
+          name: 'cBale',
+          movies: ['cBatman', 'cPrestige']
+        },
+        {
+          name: 'dBale',
+          movies: ['dBatman', 'dPrestige']
+        }
+      ],
+      myInfo: {
+        name: "Arju",
+        age: 90,
+        occ: "Megabot"
+      }
     }
   }
 }
