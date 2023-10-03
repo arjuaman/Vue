598340cefd465f751051c898e0ebe1789494ed0e
arjuaman <aman.arju427742@gmail.com>
2023-10-03 23:24:08 +0530

    Sample form

--- cli-project/vue3-vite-proj/src/App.vue
+++ cli-project/vue3-vite-proj/src/App.vue
@@ -1,29 +1,92 @@
 <template>
-  <h3 v-once>{{name}}</h3>
-  <button v-on:click="changeName">Change 1st</button>
-  <button v-on:click="changeName2">CHange 2nd</button>
+  <div class="preview">
+    <label>Name is: {{ formData.name }}</label
+    ><br />
+    <label>Summary: {{ formData.profileSummary }}</label
+    ><br />
+    <label>Country: {{ formData.country }}</label
+    ><br />
+    <label>Preferred location is: {{ formData.jobLocation }}</label
+    ><br />
+    <label>Remote? {{ formData.remote }}</label
+    ><br />
+    <label>Skills are: {{ formData.skillSet }}</label
+    ><br />
+    <label>YOE: {{ formData.yearsOfExp }}</label> <br />
+  </div>
+  <hr />
+  <div class="userInput">
+    <div>
+      <label>Enter your name: </label> <input type="text" v-model="formData.name" />
+    </div>
+    <br>
+    <div>
+      <label>Enter about yourself: </label>
+      <textarea type="text" v-model="formData.profileSummary" />
+    </div>
+    <br>
+    <div>
+      <label>Select country: </label>
+      <select v-model="formData.country">
+        <option value="India">India</option>
+        <option value="Pak">Pak</option>
+        <option value="RSA">RSA</option>
+      </select>
+    </div>
+    <br>
+    <div>
+      <label>Job location preferred: </label>
+      <select v-model="formData.jobLocation">
+        <option value="Noida">Noida</option>
+        <option value="Hyderabad">Hyderabad</option>
+        <option value="Pune">Pune</option>
+      </select>
+    </div>
+    <br>
+    <div>
+      <label>Remote preferred? </label>
+      <input type="radio" name="remote" value="Yes" v-model="formData.remote">Yes</input>
+      <input type="radio" name="remote" value="No" v-model="formData.remote">No</input>
+    </div>
+    <br>
+    <div>
+      <label>Skillsets possesed are: </label>
+      <input type="checkbox" v-model="formData.skillSet" name="skillset" value="Javascript">Javascript</input>
+      <input type="checkbox" v-model="formData.skillSet" name="skillset" value="HTML">HTML</input>
+      <input type="checkbox" v-model="formData.skillSet" name="skillset" value="Vue">Vue</input>
+      <input type="checkbox" v-model="formData.skillSet" name="skillset" value="React">React</input>
+    </div>
+    <br>
+    <div>
+      <label>What's total experience? </label>
+      <input type="number" v-model="formData.yearsOfExp"/>
+    </div>
+  </div>
+  <button type="submit" v-on:click="submitData">Submit</button>
 </template>
 
 <script>
 export default {
   data() {
-    return{
-      name: 'App',
-    }
+    return {
+      formData: {
+        name: "",
+        profileSummary: "",
+        country: "",
+        jobLocation: "",
+        remote: "",
+        skillSet: [],
+        yearsOfExp: 0,
+      },
+    };
   },
   methods: {
-    changeName(){
-      this.name = "Robin";
-    },
-    methods: {
-      changeName2() {
-        this.name = "Hawk";
-      }
+    submitData() {
+      console.log(this.formData);
     },
   },
-}
+};
 </script>
 
 <style>
-
 </style>
