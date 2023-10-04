426758107ffae06cc906c20d215686c3cbef3849
arjuaman <aman.arju427742@gmail.com>
2023-10-04 12:23:57 +0530

    Watcher

--- cli-project/vue3-vite-proj/src/App.vue
+++ cli-project/vue3-vite-proj/src/App.vue
@@ -1,50 +1,27 @@
 <template>
-  <div>Fullname using computed: {{fullName}}</div>
-  <div>Fullname using method: {{getFullName()}}</div>
-  <input type="text" name="lol" id="lol" v-model="country">
-
-  <button @click="changeName">Change Name</button>
-
-  <div v-for="i in expensiveItems" :key="i.id">{{i.name}} {{i.price}}</div>
+  <div>Current volume is: {{volume}}</div>
+  <button @click="volume+=2">increase volume</button>
+  <button @click="volume-=2">decrease volume</button>
 </template>
 
 <script>
 export default {
   data() {
     return{
-      fname: "Bruce",
-      lname: "Wayne",
-      country: "",
-      items: [
-        {id: 1, price: 50, name: "gg"},
-        {id: 2, price: 150, name: "ff"},
-        {id: 3, price: 250, name: "rr"}
-      ]
+      volume: 0
     }
   },
   methods: {
-    getFullName(){
-      console.log("inside methods");
-      return this.fname +' '+ this.lname;
-    },
-    changeName(){
-      this.fullName = "Seele Bronya"
-    }
+    
   },
   computed: {
-    fullName: {
-      get(){
-        console.log("inside computed");
-        return this.fname +' '+ this.lname;
-      },
-      set(value){
-        const name = value.split(' ');
-        this.fname = name[0];
-        this.lname = name[1];
+    
+  },
+  watch: {
+    volume(newval, oldval){
+      if(newval>oldval && newval===16){
+        console.log("VOlume high");
       }
-    },
-    expensiveItems(){
-      return this.items.filter(item => item.price>100);
     }
   }
 };
