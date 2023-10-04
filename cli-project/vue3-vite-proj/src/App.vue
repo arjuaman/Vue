905a8d184f4cad265553fb67e52a9128cdee0e72
arjuaman <aman.arju427742@gmail.com>
2023-10-04 15:10:27 +0530

    Props settings: type, default, required

--- cli-project/vue3-vite-proj/src/App.vue
+++ cli-project/vue3-vite-proj/src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <!-- <greet></greet> -->
-  <greet name="Clara" title="title"/>
-  <greet name="Clara" v-bind:title="title"/>
-  <greet name="Seele"/>
-  <greet name="Bronya"/>
-  <!-- <Greet /> -->
+  <Article v-bind:likes=50 :type="true" />
+  <Article title="Custom title" v-bind:likes=50 :type="true" />
 </template>
 
 <script>
 import Greet from "./components/Greet.vue";
+import Article from "./components/Article.vue";
 
 export default {
   components: {
     greet: Greet,
     // Greet
+    Article
   },
   data(){
     return{
---
+++ cli-project/vue3-vite-proj/src/components/Article.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div>Article Component</div>
+  <div>Likes: {{likes}}</div>
+  <div>Published? {{type}}</div>
+</template>
+
+<script>
+export default {
+  name: "Article",
+  props: {
+    title: {
+        type: String,
+        required: true,
+        default: "Principles of relativity"
+    },
+    likes: Number,
+    type: Boolean
+  },
+};
+</script>
+
+<style scoped>
+</style>
