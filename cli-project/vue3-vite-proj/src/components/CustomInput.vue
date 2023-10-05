beb6eb49bbf9dd7286977067b6f4127da1ba9ad2
arjuaman <aman.arju427742@gmail.com>
2023-10-05 11:56:59 +0530

    Solution: using v-model with custom components

--- cli-project/vue3-vite-proj/src/components/CustomInput.vue
+++ cli-project/vue3-vite-proj/src/components/CustomInput.vue
@@ -1,10 +1,12 @@
 <template>
-    <input type="text">
+    <input type="text" v-bind:value="modelValue" v-on:input="$emit('update:modelValue',$event.target.value)">
 </template>
 
 <script>
     export default {
-        
+        props:{
+            modelValue: String
+        }
     }
 </script>
 
