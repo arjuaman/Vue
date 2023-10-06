af21796b0954137b8fa9be18de1861d9de253ef9
arjuaman <aman.arju427742@gmail.com>
2023-10-06 15:18:31 +0530

    Cleaning up

--- cli-project/vue3-vite-proj/src/components/components/Data.vue
+++ cli-project/vue3-vite-proj/src/components/components/Data.vue
@@ -1,18 +1,15 @@
 <template>
-  
+    <div>
+
+    </div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  data(){
-    return{
-       
+    export default {
+        
     }
-  }
-};
 </script>
 
 <style scoped>
+
 </style>
