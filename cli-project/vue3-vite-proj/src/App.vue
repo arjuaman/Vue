a3302f3efd079e8396a739b885e2dd369af74e9c
arjuaman <aman.arju427742@gmail.com>
2023-10-09 17:31:35 +0530

    Replacing watchers in ref - single value

--- cli-project/vue3-vite-proj/src/App.vue
+++ cli-project/vue3-vite-proj/src/App.vue
@@ -1,29 +1,21 @@
 <template>
   <input type="text" v-model="fname">
-  <input type="text" v-model="lname">
-  <div>First name is: {{fname}}</div>
-  <div>Last name is: {{lname}}</div>
-  <div>Full name is: {{combo}}</div>
+  
 </template>
 
 <script> 
-import {ref, reactive, toRefs, computed} from 'vue';
+import {ref, watch, reactive, toRefs, computed} from 'vue';
 
 export default {
   name: 'App',
   setup(){
-    const state = reactive({
-      fname: '',
-      lname: ''
+    const fname = ref('');
+    watch(fname,(newVal, oldVal)=>{
+      console.log("new value is: ",newVal);
+      console.log("old value is: ",oldVal);
     })
-
-    let combo = computed(function(){
-      return `${state.fname} ${state.lname}`
-    });
-
     return{
-      ...toRefs(state),
-      combo
+      fname
     }
   }
 }
