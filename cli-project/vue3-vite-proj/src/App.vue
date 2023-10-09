b8e64b2696adecace9941eb4c756338e99bc11c6
arjuaman <aman.arju427742@gmail.com>
2023-10-09 10:35:47 +0530

    replacing data with ref

--- cli-project/vue3-vite-proj/src/App.vue
+++ cli-project/vue3-vite-proj/src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  
+  <div>{{oName}}</div>
+  <div>{{cName}}</div>
 </template>
 
-<script>
+<script> 
+import { ref } from "vue";
 
 export default {
   name: 'App',
   data(){
     return{
-       
+       oName: "Harriet"
+    }
+  },
+  setup(){
+    const cName = ref("Achilles");
+    console.log(cName);
+    return{
+      cName
     }
   }
-};
+}
 </script>
 
 <style scoped>
