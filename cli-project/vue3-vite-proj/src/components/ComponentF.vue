b0a42190a475c10a2e45fcdc2199e216f6a2f3fe
arjuaman <aman.arju427742@gmail.com>
2023-10-10 13:05:38 +0530

    misc

--- cli-project/vue3-vite-proj/src/components/ComponentF.vue
+++ cli-project/vue3-vite-proj/src/components/ComponentF.vue
@@ -1,6 +1,7 @@
 <template>
   <h2>Component F</h2>
-  <div>This is the target, getting: {{data}}</div>
+  <div>This is the target, getting: {{data}} from C</div>
+  <div>This is the target, getting: {{data1}} from A</div>
 </template>
 
 <script>
@@ -10,8 +11,10 @@ export default {
   name: 'ComponentF',
   setup(){
     const data = inject('counter', '!Error in getting!');
+    const data1 = inject('counter1', '!Error in getting!');
     return{
-      data
+      data,
+      data1
     }
   }
 }
