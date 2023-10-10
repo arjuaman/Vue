c3478d48a93443a605c2f86152ac1763a587713b
arjuaman <aman.arju427742@gmail.com>
2023-10-10 11:59:15 +0530

    Deep watchers + deep copying of objects

--- cli-project/vue3-vite-proj/src/App.vue
+++ cli-project/vue3-vite-proj/src/App.vue
@@ -7,6 +7,10 @@
     <label for="">Enter last name: </label>
     <input type="text" v-model="lname">
   </div>
+  <div>
+    <label for="">Enter hero name: </label>
+    <input type="text" v-model="options.hero">
+  </div>
 </template>
 
 <script> 
@@ -17,14 +21,18 @@ export default {
   setup(){
     const state = reactive({
       fname: '',
-      lname: ''
+      lname: '',
+      options: {
+        hero: ''
+      }
     })
 
-    watch(()=> state.fname,
+    watch(()=> JSON.parse(JSON.stringify(state.options)),
       function(newval, oldval){
         console.log("New value is: ",newval);
         console.log("Old value is: ",oldval);
-    })
+    },
+    {deep: true})
 
     return{
       ...toRefs(state)
