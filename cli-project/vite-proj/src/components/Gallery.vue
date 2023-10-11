28200f7eb34541373d43b5e0e315559f509c8e47
arjuaman <aman.arju427742@gmail.com>
2023-10-11 12:59:07 +0530

    v-img

--- cli-project/vite-proj/src/components/Gallery.vue
+++ cli-project/vite-proj/src/components/Gallery.vue
@@ -1,7 +1,9 @@
 <template>
   <v-row>
-    <v-col v-for="i in 12" cols="3">
-      <v-card> Hello </v-card>
+    <v-col v-for="i in 200" :key="i" cols="3">
+      <v-card> 
+          <v-img :src="`https://picsum.photos/500/300?image=${i * 5 + 10}`" />
+      </v-card>
     </v-col>
   </v-row>
 </template>
