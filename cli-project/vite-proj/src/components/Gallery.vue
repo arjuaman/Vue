685074e7fd2e4dd183fbcda1cc33e4f6023cace8
arjuaman <aman.arju427742@gmail.com>
2023-10-11 14:30:20 +0530

    v-img with src, lazy-src, aspect-ratio, cover; v-progress-circular with color, indeterminate

--- cli-project/vite-proj/src/components/Gallery.vue
+++ cli-project/vite-proj/src/components/Gallery.vue
@@ -1,8 +1,21 @@
 <template>
   <v-row>
-    <v-col v-for="i in 200" :key="i" cols="3">
-      <v-card> 
-          <v-img :src="`https://picsum.photos/500/300?image=${i * 5 + 10}`" />
+    <v-col v-for="i in 200" :key="i" cols="4" sm="3" md="2" lg="1">
+      <v-card>
+        <v-img
+          :src="`https://picsum.photos/500/300?image=${i * 5 + 10}`"
+          :lazy-src="`https://picsum.photos/10/10?image=${i * 5 + 10}`"
+          aspect-ratio="1"
+          cover
+        />
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey-lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
       </v-card>
     </v-col>
   </v-row>
